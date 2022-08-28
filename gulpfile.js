ade89ffd734c394ec3f8452e2b7cc3b9fc2860c9
AloisTitor <aloistitor@gmail.com>
2022-08-28 15:03:11 +0300

    Переделал галп файл. (вся эта автоматизация заняла больше времени чем вся ранее написанная вёрстка)

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,24 @@
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const sourcemap = require("gulp-sourcemaps");
+const {src, dest, watch, parallel, series} = require("gulp");
+
 const less = require("gulp-less");
-const postcss = require("gulp-postcss");
+const concat = require("gulp-concat");
+const browserSync = require("browser-sync").create();
+const terser = require("gulp-terser"); //вместо аглифи js
 const autoprefixer = require("autoprefixer");
-const sync = require("browser-sync").create();
+const plumber = require("gulp-plumber");
+const sourcemaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
+const csso = require("gulp-csso");
+const svgstore = require("gulp-svgstore");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin"); // С 8 переход на ESM
+const del = require("del"); // С 7 переход на ESM
 
-// Styles
 
-const styles = () => {
-  return gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
-    .pipe(sync.stream());
-}
-
-exports.styles = styles;
-
-// Server
-
-const server = (done) => {
-  sync.init({
+function browsersync(done) {
+  browserSync.init({
     server: {
-      baseDir: 'source'
+      baseDir: "source/"
     },
     cors: true,
     notify: false,
@@ -36,16 +26,77 @@ const server = (done) => {
   });
   done();
 }
-
-exports.server = server;
-
-// Watcher
-
-const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+function cleanBuild() {
+  return del ("build")
+}
+function sprite() {
+  return src("source/img/**/icon-*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(dest("build/img"));
+}
+function images() {
+  return src("source/img/**/*.{gif,jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ quality: 75, progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+    ]))
+    .pipe(dest("build/img"));
+}
+function scrypts() {
+  return src([
+    "node_modules/jquery/dist/jquery.js",
+    "source/js/main.js"
+  ])
+    .pipe(concat("main.min.js"))
+    .pipe(terser())
+    .pipe(dest("source/js"))
+    .pipe(browserSync.stream());
+}
+function styles() {
+  return src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(concat("style.min.css"))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("source/css"))
+    .pipe(browserSync.stream());
+}
+function copy() {
+  return src([
+    "source/css/style.min.css",
+    "source/fonts/**/*",
+    "source/js/main.min.js",
+    "source/*.html"
+  ], {base: "source"})
+    .pipe(dest("build"))
+}
+function watching() {
+  watch(["source/less/**/*.less"], styles);
+  watch(["source/js/**/*.js", "!source/js/main.min.js"], scrypts);
+  watch(["source/*.html"]).on("change", browserSync.reload);  
 }
 
-exports.default = gulp.series(
-  styles, server, watcher
-);
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.copy = copy;
+exports.scrypts = scrypts;
+exports.images = images;
+exports.sprite = sprite;
+exports.cleanBuild = cleanBuild;
+
+exports.build = series(cleanBuild, images, sprite, copy)
+exports.start = parallel(styles, scrypts, sprite, browsersync, watching);
